d6039ec47f31ac1e8e792892e4fd29cbc42a09f0
Roman Bortnikov <rb2o2.dev@gmail.com>
2018-01-27 01:05:19 +0300

    menu update

--- src/main/scala/ru/pangaia/collection/app/Application.scala
+++ src/main/scala/ru/pangaia/collection/app/Application.scala
@@ -22,7 +22,6 @@ object NewBooleanField extends NewFieldChoice
 object Back extends NewFieldChoice
 object UnknownChoice extends MenuChoice with MainMenuChoice with NewFieldChoice
 
-
 object Application
 {
   def main(args: Array[String]): Unit =
@@ -39,7 +38,7 @@ object Application
       def input(prompt: String) = StdIn.readLine(prompt).substring(0,1).toLowerCase
 
       def mainMenuChoice: MainMenuChoice =
-        input("Menu____________________\nAdd a (C)ollection\nAdd a (F)ield\nAdd a ca(R)d\n(Q)uit")
+        input("Menu____________________\nAdd a (C)ollection\nAdd a (F)ield\nAdd a ca(R)d\n(Q)uit\n")
         match
       {
         case "q" => Quit
@@ -50,7 +49,7 @@ object Application
       }
 
       def fieldChoice: NewFieldChoice =
-        input("Choose field data type: \n1 - String\n2 - Integer\n3 - Boolean(yes/no)")
+        input("Choose field data type: \n1 - String\n2 - Integer\n3 - Boolean(yes/no)\n")
         match
         {
           case "1" => NewStringField
@@ -62,35 +61,55 @@ object Application
 
       def addStringField(): Unit =
       {
-        val name = StdIn.readLine("Enter field name")
-        val desc = StdIn.readLine("Enter field description")
+        val name = StdIn.readLine("Enter field name\n")
+        val desc = StdIn.readLine("\nEnter field description\n")
         val fld = StringField(name, desc)
         coll.addField(fld)
       }
 
       def addIntField(): Unit =
       {
-        val name = StdIn.readLine("Enter field name")
-        val desc = StdIn.readLine("Enter field description")
+        val name = StdIn.readLine("Enter field name\n")
+        val desc = StdIn.readLine("\nEnter field description\n")
         val fld = IntField(name, desc)
         coll.addField(fld)
       }
 
       def addBooleanField(): Unit =
       {
-        val name = StdIn.readLine("Enter field name")
-        val desc = StdIn.readLine("Enter field description")
+        val name = StdIn.readLine("Enter field name\n")
+        val desc = StdIn.readLine("\nEnter field description\n")
         val fld = BooleanField(name, desc)
         coll.addField(fld)
       }
 
       def addCollection(): Unit =
       {
-        val name = StdIn.readLine("Enter Collection name")
-        val descr = StdIn.readLine("Enter Collection description")
+        val name = StdIn.readLine("Enter Collection name\n")
+        val descr = StdIn.readLine("\nEnter Collection description\n")
         val collection = Collection(coll, name, descr)
 
         collections += collection
+      }
+
+      def newCard(): Unit =
+      {
+        val card = CatalogCard(coll)
+        coll.fields.foreach((pair) =>
+        {
+          val fld = pair._2
+          val value = StdIn.readLine(s"\nEnter field value for field ${fld.name}\n")
+          card.writeToFieldRecord(fld)(value)
+        })
+        val collectionsMap: Map[Int, Collection] =
+          collections
+            .zipWithIndex
+            .map(pair=>(pair._2,pair._1))
+            .toMap
+        println("Which collection you want to add this card to:")
+        collectionsMap.foreach(f => println(s"(${f._1 + 1}) - ${f._2.name}"))
+        val collIndex = StdIn.readInt()
+        collections(collIndex - 1).add(card)
       }
 
       mainMenuChoice match
@@ -116,17 +135,14 @@ object Application
             }
             loop()
           }
-          case NewCard => loop()
+          case NewCard =>
+          {
+            newCard()
+            loop()
+          }
           case Quit => ()
       }
     }
-
-
-
-
-    def addCard(): Unit = ???
-
-
     loop()
   }
 }
