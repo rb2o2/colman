1d54c45fdd736054f668e19a3bc19667c1652fa1
Roman Bortnikov <rb2o2.dev@gmail.com>
2018-05-23 00:04:19 +0300

    extended Record with child classes

--- src/main/scala/ru/pangaia/collection/model/Record.scala
+++ src/main/scala/ru/pangaia/collection/model/Record.scala
@@ -2,16 +2,18 @@ package ru.pangaia.collection.model
 
 import ru.pangaia.collection.entity.{PersistentEntity, User}
 
+import scala.util.Try
+
 /**
   * Data written to a catalog card, into an associated field.
   *
   * @param field an assosiated CardField of a Collectible
   * @param user creator. Is written to <code>createdBy</code> field
   */
-case class Record(field: CardField)(implicit user: User) extends PersistentEntity
+sealed abstract class Record(field: CardField)(implicit user: User) extends PersistentEntity
 {
+  type ValueType
   override val createdBy: User = user
-  private var valu: String = field.default.toString
   def getField: CardField = field
 
   /**
@@ -19,12 +21,62 @@ case class Record(field: CardField)(implicit user: User) extends PersistentEntit
     * @param value some string conforming to the <code>field</code> semantics
     * @param user modifier. Is written to <code>modifiedBy</code> field
     */
-  def value_=(value: String)(implicit user: User): Unit =
-  {
-    this.valu = value
+  def value_=(value: ValueType)(implicit user: User): Try[Unit]
 
-    updateWithComment(user, s"value changed from: $valu to: $value")
+  def value: Option[ValueType]
+}
+
+case class StringRecord(field: StringField)(implicit user: User) extends Record(field)(user)
+{
+  override type ValueType = String
+  private var _value: Option[ValueType] = None
+
+  /**
+    * setter for the value of this Record
+    *
+    * @param value some string conforming to the <code>field</code> semantics
+    * @param user  modifier. Is written to <code>modifiedBy</code> field
+    */
+  override def value_=(value: ValueType)(implicit user: User): Try[Unit] = Try {
+    _value = Some(value)
   }
 
-  def value: String = valu
+  override def value: Option[String] = _value
+}
+
+case class IntRecord(field: IntField)(implicit user: User) extends Record(field)(user)
+{
+  override type ValueType = Int
+  private var _value: Option[ValueType] = None
+
+  /**
+    * setter for the value of this Record
+    *
+    * @param value some string conforming to the <code>field</code> semantics
+    * @param user  modifier. Is written to <code>modifiedBy</code> field
+    */
+  override def value_=(value: ValueType)(implicit user: User): Try[Unit] = Try {
+    _value = Some(value)
+  }
+
+  override def value: Option[ValueType] = _value
+}
+
+case class BooleanRecord(field: BooleanField)(implicit user: User) extends Record(field)(user)
+{
+  override type ValueType = Boolean
+
+  private var _value: Option[ValueType] = None
+
+  /**
+    * setter for the value of this Record
+    *
+    * @param value some string conforming to the <code>field</code> semantics
+    * @param user  modifier. Is written to <code>modifiedBy</code> field
+    */
+  override def value_=(value: ValueType)(implicit user: User): Try[Unit] = Try {
+    _value = Some(value)
+  }
+
+  override def value: Option[Boolean] = _value
 }
